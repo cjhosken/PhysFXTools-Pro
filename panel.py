627d8d8d3cdc9da5961976c71367e3e9bd047198
Christopher-Hosken <hoskenchristopher@gmail.com>
2021-02-27 22:47:52 +0700

    rewrite

--- panel.py
+++ panel.py
@@ -1,74 +1,152 @@
+import bpy
 from bpy.types import Panel
+from bl_ui.utils import PresetPanel
 
-class PhysFXTools_PT_Panel(Panel):
+class PhysFXToolsPro_PT_Panel(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_category = "PhysFXTools"
+    bl_category = "PhysFX Tools Pro"
 
-class PhysFXTools_PT_BasePanel(PhysFXTools_PT_Panel, Panel):
-    bl_label = 'PhysFX Tools'
-    bl_idname = "PANEL_PT_physfxtools"
+class PhysFXToolsPro_PT_BasePanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "PhysFX Tools Pro"
+    bl_idname = "PANEL_PT_physfxtoolspro"
 
     def draw(self, context):
         pass
 
-class PhysFXTools_PT_Glue_Panel(PhysFXTools_PT_Panel, Panel):
-    bl_label = 'Glue'
-    bl_idname = "PANEL_PT_physfxtoolsglue"
-    bl_parent_id = "PANEL_PT_physfxtools"
-    
+class PhysFXToolsPro_PT_GluePanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Glue"
+    bl_idname = "PANEL_PT_physfxtoolspro_glue"
+    bl_parent_id = "PANEL_PT_physfxtoolspro"
+
     def draw(self, context):
-        scene = context.scene
-        layout = self.layout
-        
-        row = layout.row()
-        layout.prop(scene.physfxtools_settings, "is_breakable", text="Breakable")
-
-        row = layout.row()
-        row.prop(scene.physfxtools_settings, "breaking_threshold", text="Threshold",)
-        row.enabled = scene.physfxtools_settings.is_breakable
-
-        row = layout.row()
-        row.prop(scene.physfxtools_settings, "glue_distance", text="Distance")
-        
-        row = layout.row()
-        row.operator("physfxtools.gluebodies", text="Glue", icon='MOD_NOISE')
-        row.operator("physfxtools.deleteglue", text="", icon='X')
-
-
-class PhysFXTools_PT_ProxyMesh_Panel(PhysFXTools_PT_Panel, Panel):
-    bl_label = 'Proxy Mesh'
-    bl_idname = "PANEL_PT_physfxtoolsproxymesh"
-    bl_parent_id = "PANEL_PT_physfxtools"
-        
-    def draw(self, context):
-        scene = context.scene
+        props = context.scene.physfxtoolspro_props
         layout = self.layout
 
         row = layout.row()
-        layout.prop(scene.physfxtools_settings, "proxy_resolution", text="Resolution")
+        row.prop(props, "glue_collection", text="Use Collection")
 
         row = layout.row()
-        layout.prop(scene.physfxtools_settings, "proxy_distance", text="Offset")
+        row.prop(props, "breaking_threshold", text="Strength")
+        
+        row = layout.row()
+        row.prop(props, "glue_distance", text="Distance")
 
         row = layout.row()
-        row.operator("physfxtools.proxymesh", text="Create Proxy", icon='MOD_MESHDEFORM')
+        row.operator("physfxtoolspro.gluebodies", text="Glue", icon="MOD_NOISE")
+        row.operator("physfxtoolspro.deleteglue", text="", icon='X')
 
-class PhysFXTools_PT_CollisionGrouping_Panel(PhysFXTools_PT_Panel, Panel):
-    bl_label = 'Collision Grouping'
-    bl_idname = "PANEL_PT_physfxtoolscollisiongrouping"
-    bl_parent_id = "PANEL_PT_physfxtools"
-    
+class PhysFXToolsPro_PT_ProxyMeshPanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Proxy Mesh"
+    bl_idname = "PANEL_PT_physfxtoolspro_proxymesh"
+    bl_parent_id = "PANEL_PT_physfxtoolspro"
+
     def draw(self, context):
+        props = context.scene.physfxtoolspro_props
+        layout = self.layout
+
+        row = layout.row()
+        row.prop(props, "proxy_resolution", text="Resolution")
+
+        row = layout.row()
+        row.prop(props, "proxy_offset", text="Offset")
+
+        row = layout.row()
+        row.operator("physfxtoolspro.makeproxymesh", text="Create Proxy", icon='MOD_MESHDEFORM')
+
+class PhysFXToolsPro_PT_GroupingBasePanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Grouping"
+    bl_idname = "PANEL_PT_physfxtoolspro_groupingbase"
+    bl_parent_id = "PANEL_PT_physfxtoolspro"
+
+    def draw(self, context):
+        pass
+
+class PhysFXToolsPro_PT_GroupCollisionsPanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Collisions"
+    bl_idname = "PANEL_PT_physfxtoolspro_groupcollisions"
+    bl_parent_id = "PANEL_PT_physfxtoolspro_groupingbase"
+
+    def draw(self, context):
+        props = context.scene.physfxtoolspro_props
+        layout = self.layout
+
+        row = layout.row()
+        col = row.column()
+        col.label(text = "Particles")
+        col.prop(props, "particle_friction", text="Friction")
+        col.prop(props, "particle_damping", text="Damping")
+        col.prop(props, "particle_random", text="Random")
+        col.prop(props, "kill_particles", text="Kill Particles")
+
+        row = layout.row()
+        col = row.column()
+        col.label(text = "Cloth")
+        col.prop(props, "cloth_friction", text="Friction")
+        col.prop(props, "cloth_damping", text="Damping")
+        col.prop(props, "cloth_thickness", text="Thickness")
+
+        row = layout.row()
+        row.operator("physfxtoolspro.groupcollisions", text="Group Collisions")
+        row.operator("physfxtoolspro.deletecollisions", text="", icon="PANEL_CLOSE")
+
+class PhysFXToolsPro_PT_GroupRigidBodiesPanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Rigid Bodies"
+    bl_idname = "PANEL_PT_physfxtoolspro_grouprigidbodies"
+    bl_parent_id = "PANEL_PT_physfxtoolspro_groupingbase"
+
+    def draw(self, context):
+        props = context.scene.physfxtoolspro_props
+        layout = self.layout
+
+        row = layout.row()
+        col = row.column()
+        col.label(text="Settings")
+        col.prop(props, 'rigidbody_shape', text="Shape")
+        col.prop(props, 'rigidbody_source', text="Source")
+        col.prop(props, 'rigidbody_mass', text="Mass")
+        col.prop(props, 'rigidbody_friction', text="Friction")
+        col.prop(props, 'rigidbody_bounce', text="Bounciness")
+        col.prop(props, 'rigidbody_margin', text="Margin")
+
+        row = layout.row()
+        row.operator("physfxtoolspro.grouprigidbodies", text="Group Rigid Bodies")
+        row.operator("physfxtoolspro.deleterigidbodies", text="", icon="PANEL_CLOSE")
+
+class PHYSFXTOOLSPRO_PT_SoftbodyPresets(PresetPanel, Panel):
+    bl_label = "Softbody Presets"
+    preset_subdir = 'physfxtoolspro/softbodies'
+    preset_operator = 'script.execute_preset'
+    preset_add_operator = 'physfxtoolspro.addsoftbodypreset'
+
+class PhysFXToolsPro_PT_SoftbodyPanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Soft Body Presets"
+    bl_idname = "PANEL_PT_physfxtoolspro_deform"
+    bl_parent_id = "PANEL_PT_physfxtoolspro"
+
+    def draw(self, context):
+        obj = context.active_object
+        softbody_active = False
+        for mod in obj.modifiers:
+            if mod.type == "SOFT_BODY":
+                softbody_active = True
+
         scene = context.scene
         layout = self.layout
 
         row = layout.row()
-        layout.prop(scene.physfxtools_settings, "collision_friction", text="Friction")
+        row.operator("physfxtoolspro.addsoftbody", text="Add Softbody")
+        row.operator("physfxtoolspro.deletesoftbody", text="", icon="PANEL_CLOSE")
 
         row = layout.row()
-        layout.prop(scene.physfxtools_settings, "collision_damping", text="Damping")
+        row.label(text="Presets: ")
+        PHYSFXTOOLSPRO_PT_SoftbodyPresets.draw_panel_header(row)
+        row.enabled = (softbody_active)
 
-        row = layout.row()
-        row.operator("physfxtools.groupcollisions", text="Group Collisions", icon="MOD_PHYSICS")
-        row.operator("physfxtools.deletegroupcollisions", text="", icon="X")
+class PhysFXToolsPro_PT_ExtraPanel(PhysFXToolsPro_PT_Panel, Panel):
+    bl_label = "Extra"
+    bl_idname = "PANEL_PT_physfxtoolspro_extra"
+    bl_parent_id = "PANEL_PT_physfxtoolspro"
+
+    def draw(self, context):
+        pass
--- props.py
+++ props.py
@@ -1,55 +1,155 @@
 from bpy.types import PropertyGroup
-from bpy.props import BoolProperty, IntProperty, FloatProperty
+from bpy.props import BoolProperty, EnumProperty, IntProperty, FloatProperty
 
-class PhysFXToolsProperties(PropertyGroup):
-    breaking_threshold: FloatProperty(
+class PhysFXToolsPro_Properties(PropertyGroup):
+    glue_collection : BoolProperty(
+        name = "Glue Collection",
+        description= "Glue objects in active collection instead of selected objects.",
+        default = False,
+    )
+
+    breaking_threshold : FloatProperty(
         name = "Breaking Threshold",
-        default = 10,
-        min = 0,
-        description = "Impluse threshold needed for the glue to break."
-    )
-    
-    glue_distance: FloatProperty(
-        name="Glue Distance",
-        default = 0.1,
-        min = 0,
-        max = 1000,
-        description = "Maximum gluing distance between selected objects"
+        description= "Threshold value used to determine the strength at which glue will break. -1 will make glue indestructable.",
+        default = -1,
+        min = -1,
     )
 
-    is_breakable: BoolProperty(
-        name="Is Breakable",
-        description="Determines whether the glue can be broken or not.",
-        default = True
-    )
-
-    proxy_distance: FloatProperty(
-        name = "Proxy Distance",
-        default = 0,
+    glue_distance : FloatProperty(
+        name = "Glue Distance",
+        description = "Max distance between glueable objects.",
+        default = 1,
         min = 0,
-        description = "Distance to keep from the target mesh."
     )
 
-    proxy_resolution: IntProperty(
+    proxy_resolution : IntProperty(
         name = "Proxy Resolution",
+        description = "Resolution of proxy mesh.",
+        default = 3,
+        min = 1,
+        max = 6,
+    )
+
+    proxy_offset : FloatProperty(
+        name = "Proxy Offset",
+        description = "Amount of space between base mesh and proxy.",
+        default = 0,
+        min = 0,
+    )
+
+    particle_friction : FloatProperty(
+        name = "Particle Friction",
+        description = "Amount of friction during particle collision.",
+        min = 0,
+        max = 1,
+        default = 0,
+    )
+
+    particle_damping : FloatProperty(
+        name = "Particle Damping",
+        description = "Amount of damping during particle collision.",
+        min = 0,
+        max = 1,
+        default = 0,
+    )
+
+    particle_random : FloatProperty(
+        name = "Particle Random",
+        description = "Random variation of friction and damping",
+        min = 0,
+        max = 1,
+        default = 0,
+    )
+
+    kill_particles : BoolProperty(
+        name = "Kill Particles",
+        description = "Kill collided particles.",
+        default = False,
+    )
+
+    cloth_friction : FloatProperty(
+        name = "Cloth Friction",
+        description = "Friction for cloth collisions",
+        min = 0,
+        max = 80,
         default = 5,
-        min = 0,
-        max = 10,
-        description = "Resolution of the proxy mesh."
     )
-    
-    collision_friction: FloatProperty(
-        name="Collision Friction",
-        default = 0,
+
+    cloth_damping : FloatProperty(
+        name = "Cloth Damping",
+        description = "Amount of damping for cloth collisions",
         min = 0,
         max = 1,
-        description = "Amount of friction for all objects in the selected group."
-    )
-    
-    collision_damping: FloatProperty(
-        name="Collision Damping",
         default = 0,
+    )
+
+    cloth_thickness : FloatProperty(
+        name = "Thickness",
+        description = "Outer thickness of object.",
         min = 0,
         max = 1,
-        description = "Amount of collision damping for all objects in the selected group."
+        default = 0.02,
+    )
+
+    rigidbody_active : BoolProperty(
+        name = "Rigidbody Type",
+        description = "Role of object in rigidbody simulations.",
+        default = True,
+    )
+
+    rigidbody_mass : FloatProperty(
+        name = "Rigidbody Mass",
+        description = "",
+        min = 0,
+        default = 1,
+    )
+
+    rigidbody_shape : EnumProperty(
+        name = "Rigidbody Shape",
+        description="",
+        items = [
+            ("COMPOUND", "Compound Parent", ""),
+            ("MESH", "Mesh", ""),
+            ("CONVEX_HULL", "Convex Hull", ""),
+            ("CONE", "Cone", ""),
+            ("CYLINDER", "Cylinder", ""),
+            ("CAPSULE", "Capsule", ""),
+            ("SPHERE", "Sphere", ""),
+            ("BOX", "Box", ""),
+        ],
+        default="CONVEX_HULL",
+    )
+
+    rigidbody_source : EnumProperty(
+        name = "Rigidbody Shape",
+        description="",
+        items = [
+            ("BASE", "Base", ""),
+            ("DEFORM", "Deform", ""),
+            ("FINAL", "Final", ""),
+        ],
+        default = "BASE",
+    )
+
+    rigidbody_friction : FloatProperty(
+        name = "Rigidbody Friction",
+        description = "Resistance of object to movement.",
+        min = 0,
+        max = 1,
+        default = 0.5,
+    )
+
+    rigidbody_bounce : FloatProperty(
+        name = "Rigidbody Bounciness",
+        description = "Tendency of object to bounce after collision (0 = stays still, 1 = perfectly elastic).",
+        min = 0,
+        max = 1,
+        default = 0.5,
+    )
+
+    rigidbody_margin : FloatProperty(
+        name = "Rigidbody Margin",
+        description = "Threshold of distance where collisions are still considered.",
+        min = 0,
+        default = 0.04,
     )
